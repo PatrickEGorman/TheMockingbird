57370fb613cb7a7ee422758fd192bfd982e72f69
PatrickEGorman <gormanpe@vcu.edu>
2019-06-09 16:20:08 -0400

    Created limit on title size input

--- controllers/articles.js
+++ controllers/articles.js
@@ -5,6 +5,10 @@ exports.create_article_page = function(req, res, next) {
 };
 
 exports.create_article = function(req, res, next) {
+    if(req.body.title.length > 100){
+        return res.render('article/create_article', { title: 'Create Article',
+            error:'Article title length must be under 100 characters'});
+    }
     const createArticleWithMessages = async () => {
         const newArticle = new Article.Article({
             title : req.body.title,
@@ -22,7 +26,11 @@ exports.create_article = function(req, res, next) {
 exports.view_articles = function (req, res, next) {
     Article.Article.find({}, function(err, articles) {
         if (err) throw err;
-
+        for(let i=0; i<articles.length; i++){
+            if(articles[i].contents.length > 250){
+                articles[i].contents = articles[i].contents.slice(0, 249) + "...";
+            }
+        }
         return res.render('article/list_articles', {title: 'View Articles', articles: articles})
     })
 };
